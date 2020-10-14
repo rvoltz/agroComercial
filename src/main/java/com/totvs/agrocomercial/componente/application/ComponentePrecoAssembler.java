3cdda8a13549798ed9d8e0fb41db204570d3620b
Wagner Costa <wg.o.costa@gmail.com>
2020-10-14 09:53:42 -0300

    correção build

--- src/main/java/com/totvs/agrocomercial/componente/application/ComponentePrecoAssembler.java
+++ src/main/java/com/totvs/agrocomercial/componente/application/ComponentePrecoAssembler.java
@@ -3,25 +3,23 @@ package com.totvs.agrocomercial.componente.application;
 import com.totvs.agrocomercial.commons.base.application.EntityDTOAssembler;
 import com.totvs.agrocomercial.componente.domain.ComponenteMapper;
 import com.totvs.agrocomercial.componente.domain.ComponentePreco;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 
 public class ComponentePrecoAssembler implements EntityDTOAssembler<ComponentePreco, ComponentePrecoRequestDTO, ComponentePrecoResponseDTO> {
 
-    @Autowired
-    private ComponenteMapper componenteMapper;
-
+    //private final ComponenteMapper componenteMapper = ComponenteMapper.INSTANCE;
 
     @Override
     public ComponentePrecoResponseDTO fromEntity(ComponentePreco entity) {
-        return componenteMapper.toDTO(entity);
-        //return new ComponentePrecoResponseDTO(entity.getId(), entity.getCodigo(), entity.getDescricao());
+        //System.out.println(componenteMapper.toDTO(entity));
+        return new ComponentePrecoResponseDTO(entity.getId(), entity.getCodigo(), entity.getDescricao());
     }
 
     @Override
     public ComponentePreco fromDTO(ComponentePrecoRequestDTO dto) {
-        return componenteMapper.toModel(dto);
-//        return new ComponentePreco(dto.getCodigo(), dto.getDescricao(), dto.getCodigoExterno(), dto.getUnidadeMedida(), dto.getEnumMoeda(),
-//                dto.getTipo(), dto.getTabelaPreco(), dto.getEnumAplicacao(), dto.isHedge(), dto.isAtivo(), dto.getFinalidades(), dto.getTiposFrete(), dto.getComponentes(), dto.getItens());
-//
+        //System.out.println(componenteMapper.toModel(dto));
+        return new ComponentePreco(dto.getCodigo(), dto.getDescricao(), dto.getCodigoExterno(), dto.getUnidadeMedida(), dto.getEnumMoeda(),
+                dto.getTipo(), dto.getTabelaPreco(), dto.getEnumAplicacao(), dto.isHedge(), dto.isAtivo(), dto.getFinalidades(), dto.getTiposFrete(), dto.getComponentes(), dto.getItens());
+
     }
 }
